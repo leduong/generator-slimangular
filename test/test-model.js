d9eed577b4b19559b2cfa93e82cdf9357633b32f
Le Duong <me@leduong.com>
2014-09-21 14:08:48 +0700

    update for travis

---
+++ test/test-model.js
@@ -0,0 +1,16 @@
+/*global describe, beforeEach, it*/
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+var os = require('os');
+
+describe('slimangular:model', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../model'))
+      .inDir(path.join(os.tmpdir(), './temp-test'))
+      .withArguments('name', '--force')
+      .on('end', done);
+  });
+});
